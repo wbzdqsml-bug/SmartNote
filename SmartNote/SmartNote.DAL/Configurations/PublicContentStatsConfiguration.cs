ce499006bdd80b900c2fde82e55028cb43c00f7e
wbzdqsml <120867673+wbzdqsml-bug@users.noreply.github.com>
2026-01-20 11:08:18 +0800

    Avoid cascade delete loop on task logs

---
+++ SmartNote/SmartNote.DAL/Configurations/PublicContentStatsConfiguration.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartNote.Domain.Entities;
+
+namespace SmartNote.DAL.Configurations
+{
+    public class PublicContentStatsConfiguration : IEntityTypeConfiguration<PublicContentStats>
+    {
+        public void Configure(EntityTypeBuilder<PublicContentStats> builder)
+        {
+            builder.ToTable("PublicContentStats");
+            builder.HasKey(stats => stats.PublicContentId);
+            builder.HasQueryFilter(stats => !stats.PublicContent.Note.IsDeleted);
+        }
+    }
+}
